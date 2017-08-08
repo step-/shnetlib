c04988b955475f3b5da7f564352d983b91b54dae
step- <step-@users.noreply.github.com>
2017-08-08 09:25:20 +0200

    Add IFACE_<list>_iface - list of basenames.

--- usr/sbin/shnetlib.sh
+++ usr/sbin/shnetlib.sh
@@ -8,13 +8,14 @@
 set +f
 
 # {{{1}}}
-# <path> ::= paths '/sys/class/net/'*
-# <iface> ::= basenames '/sys/class/net/'*
+# <path> ::= path in '/sys/class/net/'*
+# <iface> ::= basename in '/sys/class/net/'*
 # <list> ::= 'other' | 'wireless' | 'wired'
 # <list_which> ::= <integer> >= 0
 # IFACE_<list>_n ::= '0' .. number of <list> interfaces detected
 # IFACE_<list>_which ::= list of <list_which> (enumeration index [0..IFACE_<list>_n - 1])
-# IFACE_<list>_path ::= <path>
+# IFACE_<list>_path ::= list of <path>
+# IFACE_<list>_iface ::= list of <iface>
 # IFACE_<list>_bus ::= list of 'NA' | 'pci' | 'usb' | ...
 # Similarly as above for each member of <list>.
 # IFACE_wireless_phy ::= list of 'phy'<integer>
@@ -32,6 +33,7 @@ enum_interfaces() # {{{1
 	IFACE_other_n=0 IFACE_wired_n=0 IFACE_wireless_n=0
 	unset IFACE_other_which IFACE_wired_which IFACE_wireless_which
 	unset IFACE_other_path IFACE_wired_path IFACE_wireless_path
+	unset IFACE_other_iface IFACE_wired_iface IFACE_wireless_iface
 	unset IFACE_other_bus IFACE_wired_bus IFACE_wireless_bus
 	unset IFACE_wireless_phy IFACE_wireless_rfkill_index
 	BUS_other_n=0 BUS_pci_n=0 BUS_usb_n=0
@@ -47,6 +49,7 @@ enum_interfaces() # {{{1
 				IFACE_other_which="$IFACE_other_which $IFACE_other_n"
 				IFACE_other_n=$(($IFACE_other_n + 1))
 				IFACE_other_path="$IFACE_other_path $p"
+				IFACE_other_iface="$IFACE_other_iface ${p##*/}"
 				IFACE_other_bus="$IFACE_other_bus $bus"
 				;;
 			*)
@@ -55,6 +58,7 @@ enum_interfaces() # {{{1
 					IFACE_wireless_which="$IFACE_wireless_which $IFACE_wireless_n"
 					IFACE_wireless_n=$(($IFACE_wireless_n + 1))
 					IFACE_wireless_path="$IFACE_wireless_path $p"
+					IFACE_wireless_iface="$IFACE_wireless_iface ${p##*/}"
 					IFACE_wireless_bus="$IFACE_wireless_bus $bus"
 					read x < $p/phy80211/name
 					IFACE_wireless_phy="$IFACE_wireless_phy ${x:-NA}"
@@ -65,6 +69,7 @@ enum_interfaces() # {{{1
 					IFACE_wired_which="$IFACE_wired_which $IFACE_wired_n"
 					IFACE_wired_n=$(($IFACE_wired_n + 1))
 					IFACE_wired_path="$IFACE_wired_path $p"
+					IFACE_wired_iface="$IFACE_wired_iface ${p##*/}"
 					IFACE_wired_bus="$IFACE_wired_bus $bus"
 				fi
 				;;
@@ -92,7 +97,7 @@ enum_interfaces() # {{{1
 # IFACE_list ::= 'other' | 'wireless' | 'wired'
 # IFACE_which ::= '0' .. IFACE_<IFACE_list>_n - 1
 # IFACE_path ::= <IFACE_which>'th element of IFACE_<IFACE_list>_path
-# IFACE_iface ::= basename of IFACE_path
+# IFACE_iface ::= <IFACE_which>'th element of IFACE_<IFACE_list>_iface (basename of IFACE_path)
 # IFACE_bus ::= 'NA' | <IFACE_which>'th element of IFACE_<IFACE_list>_bus
 # Ditto for IFACE_phy and IFACE_rfkill_index when IFACE_list == 'wireless'
 
@@ -112,7 +117,7 @@ get_iface_other() # [--export] $1-which {{{1
 	[ $which = error -o $which -gt $IFACE_other_n ] && return 1
 	$opt_e IFACE_list=other # begin
 	set -- $IFACE_other_path; shift $which; $opt_e IFACE_path=$1
-	$opt_e IFACE_iface=${1##*/}
+	set -- $IFACE_other_iface; shift $which; $opt_e IFACE_iface=$1
 	set -- $IFACE_other_bus; shift $which; $opt_e IFACE_bus=$1
 	$opt_e IFACE_which=$which # end
 }
@@ -126,7 +131,7 @@ get_iface_wireless() # [--export] $1-which {{{1
 	[ $which = error -o $which -gt $IFACE_wireless_n ] && return 1
 	$opt_e IFACE_list=wireless # begin
 	set -- $IFACE_wireless_path; shift $which; $opt_e IFACE_path=$1
-	$opt_e IFACE_iface=${1##*/}
+	set -- $IFACE_wireless_iface; shift $which; $opt_e IFACE_iface=$1
 	set -- $IFACE_wireless_bus; shift $which; $opt_e IFACE_bus=$1
 	set -- $IFACE_wireless_phy; shift $which; $opt_e IFACE_phy=$1
 	set -- $IFACE_wireless_rfkill_index; shift $which; $opt_e IFACE_rfkill_index=$1
@@ -143,7 +148,7 @@ get_iface_wired() # [--export] $1-which {{{1
 	[ $which = error -o $which -gt $IFACE_wired_n ] && return 1
 	$opt_e IFACE_list=wired # begin
 	set -- $IFACE_wired_path; shift $which; $opt_e IFACE_path=$1
-	$opt_e IFACE_iface=${1##*/}
+	set -- $IFACE_wired_iface; shift $which; $opt_e IFACE_iface=$1
 	set -- $IFACE_wired_bus; shift $which; $opt_e IFACE_bus=$1
 	$opt_e IFACE_which=$which # end
 }
